eafe491a0c37be2330f331a0edb9666ab16e1d2d
Thu Hang Nguyen <hangnguyenrpd170@gmail.com>
2019-06-07 15:36:59 +0700

    Update to main folder

--- build-RollingGame-Desktop_Qt_5_12_0_MSVC2017_64bit-Debug/moc/moc_predefs.h
+++
@@ -1,8 +0,0 @@
-# 1 "C:\\Qt\\5.12.0\\msvc2017_64\\mkspecs\\features\\data\\dummy.cpp"
-# 1 "<built-in>" 1
-# 1 "<built-in>" 3
-# 371 "<built-in>" 3
-# 1 "<command line>" 1
-# 1 "<built-in>" 2
-# 1 "C:\\Qt\\5.12.0\\msvc2017_64\\mkspecs\\features\\data\\dummy.cpp" 2
-
